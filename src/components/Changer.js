1da6f4892a8a1f8909e63e1149627b4c0879302d
unknown <sayedthone1@gmail.com>
2021-06-04 19:49:21 +0200

    fixed checkmark none option

--- src/components/Changer.js
+++ src/components/Changer.js
@@ -18,9 +18,7 @@ const Changer = ({ shelfChanger, book, books }) => {
   return (
     <div className="book-shelf-changer">
       <select onChange={changeShelf} defaultValue={currentShelf}>
-        <option value="none" disabled>
-          Move to...
-        </option>
+        <option disabled>Move to...</option>
         <option value="currentlyReading">Currently Reading</option>
         <option value="wantToRead">Want to Read</option>
         <option value="read">Read</option>
